04f4efbe4fad5f2fc5054c8671cce917a3f94429
freakGE <esebua154@gmail.com>
2023-02-15 21:38:20 +0400

    APP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { AppStateProvider } from "@/components/AppState";
+import { useRouter } from "next/router";
+
+const Header = dynamic(() => import("../components/Header"));
+const Footer = dynamic(() => import("../components/Footer"));
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const layoutArray = ["/auth/signin", "/signup"];
+  const showLayout = !layoutArray.includes(router.pathname);
+  return (
+    <AppStateProvider>
+      <SessionProvider session={pageProps.session}>
+        {showLayout && <Header />}
+        <Component {...pageProps} />
+        {showLayout && <Footer />}
+      </SessionProvider>
+    </AppStateProvider>
+  );
 }
