e42a91417ada9a543c9b462f3d41c03a28c1295b
freakGE <esebua154@gmail.com>
2024-10-24 00:31:31 +0400

    Changed loading from lazy to eager & minor improv

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import Image, { ImageProps } from "next/image";
-import { useState } from "react";
+import { memo, useState } from "react";
 
 const Spinner = dynamic(() => import("./Spinner"));
 
@@ -25,7 +25,7 @@ const LazyImage = ({
   ...rest
 }: LazyImageProps): JSX.Element => {
   const [isLoading, setIsLoading] = useState(true);
-  const [placeholder, setPlaceholder] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   return (
     <div className="relative flex h-full w-full items-center justify-center text-dark-100">
@@ -39,48 +39,35 @@ const LazyImage = ({
           />
         </div>
       )}
-      {placeholder && (
-        <div className="absolute z-[1] flex h-full w-full items-center justify-center bg-dark-150 duration-[250ms] hover:scale-105">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            style={{
-              width: !!navbar
-                ? spinnerSize + "rem"
-                : spinnerSize * 1.75 + "rem",
-            }}
-            strokeWidth="0"
-            viewBox="0 0 16 16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
-            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"></path>
-          </svg>
-        </div>
-      )}
       {isLoading && children}
       <Image
         src={src}
         alt={alt || "Loading..."}
-        quality={isLoading ? 1 : 100}
+        quality={100}
+        // quality={isLoading ? 1 : 100}
         width={500}
         height={500}
-        loading="lazy"
-        style={{ visibility: isLoading ? "hidden" : "visible" }}
+        priority
+        loading="eager"
         onLoadingComplete={() => {
           setIsLoading(false);
-          setPlaceholder(false);
           onImageLoad && onImageLoad();
         }}
         onError={() => {
           setIsLoading(false);
-          setPlaceholder(true);
+          setHasError(true);
           onImageLoad && onImageLoad();
         }}
+        className="flex justify-center items-center bg-red-200"
         {...rest}
       />
+           {hasError && (
+        <div className="absolute z-[1] flex h-full w-full items-center justify-center bg-dark-150">
+          <p className="text-dark-100 text-sm font-semibold">Failed to load image.</p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default LazyImage;
+export default memo(LazyImage);
